956b202350b73b7d786fe6bcec0649d573861587
boyeong <boyeong.mdlog@gmail.com>
2024-08-28 10:56:15 +0900

    20240828 header

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 function Header() {
+    const [menu, setMenu] = useState("close");
+    const openMenu = () => {
+        setMenu("open");
+    }
+    const closeMenu = () => {
+        setMenu("close");
+    }
+    const [menuIdx, setMenuIdx] = useState(0);
+    const handleMenuIdx = (mIdx) => {
+        setMenuIdx(mIdx)
+    }
+
+    const [headerOver, setHeaderOver] = useState("off");
+    const handleHeaderOn = (idx) => {
+        setHeaderOver("on");
+        handleMenuIdx(idx);
+    }
+    const handleHeaderOut = () => {
+        setHeaderOver("off")
+    }
+
     return (
         <>
             <header>
-                <div className="headerWrap">
-                    <div className="headerInner">
+                <div className={`headerWrap ${headerOver}`}>
+                    <div className={`headerInner ${menu}`}>
                         <h1>화물잇고</h1>
                         <nav>
-                            <ul>
-                                <li><Link to="#">서비스 소개</Link></li>
-                                <li>
-                                    <Link to="#">공지사항</Link>
+                            <div className="responseBar" onClick={openMenu}>
+                                <span className="bar"></span>
+                                <span className="bar"></span>
+                                <span className="bar"></span>
+                            </div>
+                            <ul className={`menuOn${menuIdx}`}>
+                                <li className="close">
+                                    <button onClick={closeMenu}></button>
+                                </li>
+                                <li onMouseEnter={()=>handleHeaderOn(1)} onMouseLeave={handleHeaderOut}>
+                                    <Link to="#">서비스 소개</Link>
+                                    <em></em>
                                     <p>
-                                        <span></span>
+                                        <Link to="#">차주</Link>
+                                        <Link to="#">주선사</Link>
+                                        <Link to="#">화주</Link>
+                                    </p>
+                                </li>
+                                <li onMouseEnter={()=>handleHeaderOn(2)} onMouseLeave={handleHeaderOut}>
+                                    <Link to="#">공지사항</Link>
+                                    <em></em>
+                                    <p>
                                         <Link to="#">공지사항</Link>
                                         <Link to="#">이벤트</Link>
                                     </p>
                                 </li>
-                                <li>
+                                <li onMouseEnter={()=>handleHeaderOn(3)} onMouseLeave={handleHeaderOut}>
                                     <Link to="#">자주묻는 질문</Link>
+                                    <em></em>
                                     <p>
-                                        <span></span>
                                         <Link to="#">전체</Link>
                                         <Link to="#">차주</Link>
                                         <Link to="#">주선사</Link>
@@ -30,7 +67,8 @@ function Header() {
                                 </li>
                             </ul>
                         </nav>
-                        <button>주선사 바로가기</button>
+                        <button className="goTo">주선사 바로가기</button>
+                        <div className="responseBg"></div>
                     </div>
 
                 </div>
