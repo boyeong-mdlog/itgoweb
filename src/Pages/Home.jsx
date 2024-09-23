b6cf5326b81bf1fa4e40abb2144b1a1bb2d1601e
boyeong <boyeong.mdlog@gmail.com>
2024-09-23 14:31:31 +0900

    작업중

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,14 @@
-import React, {useState, useLayoutEffect, useRef, useEffect} from "react";
-import {useSearchParams} from "react-router-dom";
+import React, {useRef, useState, useEffect} from "react";
 import oneVideo from "../res/images/video_1.mp4";
 import twoVideo from "../res/images/video_2.mp4";
-import {MainSvgOne, MainSvgTwo, MainSvgThree} from "../components/svg/MainSvg";
+import {MainSvgOne, MainSvgTwo, MainSvgThree, MainLogo} from "../components/svg/MainSvg";
 import HomeTabContent from "./HomeTabContent";
 
 import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useGSAP } from '@gsap/react';
+gsap.registerPlugin(useGSAP);
 
 let vidx = 0;
-let midx = 0;
 
 const videoSource = [
     {
@@ -23,53 +21,38 @@ const videoSource = [
     }
 ]
 
-const topMainContent= [
+const contents= [
     {
         type: 'video',
-        source: videoSource[vidx],
+        component: videoSource[vidx].source,
         title: '화물을 넘어, 사람과 사람을 이어주는',
         subtitle: '혁신적인 디지털 주선 업무와 맞춤형 스마트 배차 플랫폼',
-        duration: videoSource[vidx],
+        duration: videoSource[vidx].duration,
     },
     {
         type: 'svg',
-        component: MainSvgOne,
+        component: <MainSvgOne />,
         title: '차주님에게 필요한',
         subtitle: '여러 화물도 한 번에',
         duration: 5000,
     },
     {
         type: 'svg',
-        component: MainSvgTwo,
+        component: <MainSvgTwo />,
         title: '화물 등록에서 배차, 운임료 지급까지',
         subtitle: '간편 화물 주선 토탈 관리',
         duration: 5000,
     },
     {
         type: 'svg',
-        component: MainSvgThree,
+        component: <MainSvgThree />,
         title: '전화는 그만',
         subtitle: '앱에서 보는 실시간 화물 트래킹',
         duration: 5000,
     },
 ]
-gsap.registerPlugin(useGSAP, ScrollTrigger);
 
-const MainInit = () => {
-    return (
-        <></>
-    )
-}
-
-function updateContent() {
-    midx++;
-    if(midx > topMainContent.length) {midx = 0;}
-    vidx++;
-    if(vidx === 3) {vidx =0;}
-}
 function Home() {
-
-
     let scrollTop;
     window.addEventListener(
         "scroll",
@@ -94,17 +77,190 @@ function Home() {
         { passive: true }
     );
 
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const onVideoEnded = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % contents.length);
+    };
+
+
+    const mainMedia = useRef();
+    const mainText = useRef();
+
+    useGSAP(
+        () =>{
+            gsap.from('.main-video-title', {
+                scrollTrigger: {
+                    trigger: '.introduction-main',
+                    once: true,
+                    markers: false,
+                },
+                ease: 'power3.out',
+                duration: 0.7,
+                opacity: 0,
+                y: 20,
+            })
+            gsap.from('.main-video-subtitle', {
+                scrollTrigger: {
+                    trigger: '.introduction-main',
+                    once: true,
+                    markers: false,
+                },
+                ease: 'power3.out',
+                delay: 5.2,
+                duration: 0.7,
+                opacity: 0,
+                y: 10,
+            })
+
+            const t1 = gsap.timeline({
+                scrollTrigger: {
+                    trigger: '.introduction-main',
+                    once: true,
+                    markers: false,
+                },
+            })
+            t1.from('.logo_first_text', {
+                ease: 'power3.out',
+                delay: 1.2,
+                duration: 0.6,
+                opacity: 0,
+                x: -30,
+                scale: 1.05,
+            })
+            t1.from('.logo_second_text', {
+                ease: 'power3.out',
+                delay: 0.3,
+                duration: 0.6,
+                opacity: 0,
+                y: -20,
+                scaleY: 1.1,
+            })
+            t1.from('.logo_third_text', {
+                ease: 'power3.out',
+                delay: 0.3,
+                duration: 0.6,
+                opacity: 0,
+                y: 5,
+                scale: 1.2,
+            })
+            t1.from('.logo_fourth_two', {
+                ease: 'power3.out',
+                delay: 0.3,
+                duration: 0.6,
+                opacity: 0,
+                x: -30,
+                scale: 1.05,
+            })
+            t1.from('.logo_magenta_text', {
+                ease: 'power3.out',
+                delay: 0.2,
+                duration: 0.8,
+                opacity: 0,
+                x: 10,
+                y: -10,
+                scale: 1.05,
+            })
+
+            gsap.from('.logo_fourth_one', {
+                scrollTrigger: {
+                    trigger: '.introduction-main',
+                    once: true,
+                    markers: false,
+                },
+                ease: 'power3.out',
+                delay: 4.1,
+                duration: 0.5,
+                opacity: 0,
+                x: 10,
+                y: -10,
+                scale: 1.1,
+            })
+
+        },{scope: mainMedia}
+    );
+
+    useGSAP(
+        ()=>{
+            gsap.from('.main-svg-title', {
+                scrollTrigger: {
+                    trigger: '.introduction-main',
+                    once: true,
+                    markers: false,
+                },
+                ease: 'power2.in',
+                duration: 0.5,
+                opacity: 0,
+                scale: 1.1,
+            })
+            gsap.from('.main-svg-subtitle', {
+                scrollTrigger: {
+                    trigger: '.introduction-main',
+                    once: true,
+                    markers: false,
+                },
+                ease: 'power2.in',
+                delay: 1,
+                duration: 0.3,
+                opacity: 0,
+                scale: 1.02,
+            })
+        },{scope: mainText}
+    );
+
+
+
+    useEffect(()=>{
+
+    });
+
+    const UpdateContent = () => {
+        setCurrentIndex(currentIndex > contents.length - 1 ? 0 : currentIndex+1);
+    }
+
+
 
     return (
         <div id="introduction">
             <div className="introduction-main">
-                <div className="introduction-media">
-                    <MainInit />
+                <div className="introduction-media" ref={mainMedia}>
+                    {contents[currentIndex].type === 'video' ? (
+                        <video
+                            key={`${currentIndex}-video`}
+                            autoPlay
+                            muted
+                            playsInline
+                            preload="auto"
+                            onEnded={onVideoEnded}
+                        >
+                            <source src={contents[currentIndex].component} type="video/mp4"/>
+                        </video>
+                    ) : contents[currentIndex].type === 'svg' ? (
+                        <>{contents[currentIndex].component}</>
+                    ) : null}
                 </div>
-                <div className="introduction-main-text">
+                <div className="introduction-main-text" ref={mainText}>
+                    {contents[currentIndex].type === 'video' ? (
+                        <>
+                            <div className="main-video-title">{contents[currentIndex].title}</div>
+                            <div className="main-logo">
+                                <MainLogo />
+                            </div>
+                            <div className="main-video-subtitle">
+                                {contents[currentIndex].subtitle}
+                            </div>
+                        </>
+                    ) : contents[currentIndex].type === 'svg' ? (
+                        <>
+                            <div className="main-svg-title">{contents[currentIndex].title}</div>
+                            <div className="main-svg-subtitle">
+                                {contents[currentIndex].subtitle}
+                            </div>
+                        </>
+                    ): null}
                 </div>
             </div>
-            <HomeTabContent />
+           {/* <HomeTabContent/>*/}
         </div>
 
     );
